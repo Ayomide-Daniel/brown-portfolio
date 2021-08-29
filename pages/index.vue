c2a4b52f3a1d47145ebb22a48b44861716fa8233
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2021-08-29 12:53:40 +0100

    feat: section-1

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
-    <section class="cream d-flex">
-      <div>
-        <h1>
-          Holla 👋, I’m
-          <span class="brown-text text-decoration-underline">Brown!</span>
+    <section class="cream-color d-flex section-1 container">
+      <div class="meta-div">
+        <h1 class="lg-text">
+          Holla 👋, <br />
+          I’m
+          <span class="brown-text text-decoration-underline"
+            >Brown!
+            <!-- <span class=" underline-div"></span> -->
+          </span>
         </h1>
+        <p><small>“As you dey here so, I don sight you”</small></p>
+      </div>
+      <div class="img-div">
+        <img :src="require('~/assets/images/me-on-blue.png')" alt="" />
       </div>
     </section>
-    <section class="green d-flex">
+    <section class="green-color d-flex section-2 container">
       <div>
         <h1>
-          Holla 👋, I’m
-          <span class="brown-text text-decoration-underline">Brown!</span>
+          "Think differently. <br />
+          Innovate differently. <br />
+          Provide solutions rather than discuss problems. <br />
+          And while you do these things, remember that your Greatest Strength
+          lies in doing things according to your unique abilities." <br />
+          These are the words I live and breathe by and I use Web Development as
+          one of my tools to continually affirm these beliefs.
         </h1>
       </div>
     </section>
